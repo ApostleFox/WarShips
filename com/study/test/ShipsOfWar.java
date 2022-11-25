bfad2d1063c3a45d65bb5b77b3f9a1e4dcd8f53c
ApostleFox <Aazik-teorsik@ukr.net>
2022-11-25 18:17:50 +0200

    feat: add test

---
+++ com/study/test/ShipsOfWar.java
@@ -0,0 +1,32 @@
+package com.study.test;
+
+import java.util.Scanner;
+
+
+
+public class ShipsOfWar  {
+
+     public static void main(String[] args) {
+//           Ship ship = new Ship();
+//            Scan scan = new Scan();
+//
+//            System.out.println("Ведіть розмір поля");
+//            Scanner readShipLength = new Scanner(System.in);
+//            ship.setShipLength(readShipLength(scan));
+//
+//
+//            System.out.println("Ведіть розмір коробля");
+//            Scanner sizeShip = new Scanner(System.in);
+//            ship.setShipLength(sizeShip.nextInt());
+//
+//            ship.ship();
+//
+//        }
+//
+//    private static int readShipLength(Scan scan) {
+//
+//        return 0;
+            System.out.println("Hello World!");
+    }
+}
+
