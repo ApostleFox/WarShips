50ca13f0b3367582085ad6e002948f1e985014b3
ApostleFox <Aazik-teorsik@ukr.net>
2022-11-27 14:40:31 +0200

    feat: add test

--- com/study/test/ShipsOfWar.java
+++ com/study/test/ShipsOfWar.java
@@ -5,28 +5,53 @@ import java.util.Scanner;
 
 
 public class ShipsOfWar  {
+    Ship ship = new Ship();
+    private static int readShipLength(Scanner scan) {
+        while (true) {
+            int value = scan.nextInt();
+            if (value > 0 & value <= 100) {
+                return value;
+
+            } else {
+                System.out.println("Write a number from 1 to 100");
+            }
+        }
+    }
+
+    private static int readFieldLength(Scanner scan) {
+        while (true) {
+            int value = scan.nextInt();
+            if (value > 0 & value <= 100) {
+                return value;
+
+            } else {
+                System.out.println("Write a number from 1 to 100");
+            }
+        }
+    }
+
 
      public static void main(String[] args) {
-//           Ship ship = new Ship();
-//            Scan scan = new Scan();
-//
-//            System.out.println("Ведіть розмір поля");
-//            Scanner readShipLength = new Scanner(System.in);
-//            ship.setShipLength(readShipLength(scan));
-//
-//
-//            System.out.println("Ведіть розмір коробля");
-//            Scanner sizeShip = new Scanner(System.in);
-//            ship.setShipLength(sizeShip.nextInt());
-//
-//            ship.ship();
-//
-//        }
-//
-//    private static int readShipLength(Scan scan) {
-//
-//        return 0;
-            System.out.println("Hello World!");
-    }
+         Ship ship = new Ship();
+
+         System.out.println("Ведіть розмір поля");
+         readFieldLength(new Scanner(System.in));
+         ship.setShipLength(readFieldLength(new Scanner(System.in)));
+
+         System.out.println("Ведіть розмір поля");
+         readShipLength(new Scanner(System.in));
+         ship.setShipLength(readShipLength(new Scanner(System.in)));
+
+
+         ship.ship();
+
+        }
+
+
+
+
 }
+
+
+
 
