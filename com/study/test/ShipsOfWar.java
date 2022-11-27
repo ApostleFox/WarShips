9d678869060c2a35a00e19dfb2a67fe03efbe35b
ApostleFox <Aazik-teorsik@ukr.net>
2022-11-27 20:04:04 +0200

    feat: Проблема з зберіганням(або можливістю звернення) даних першого скану(в подальшому другому скані я не маю можливості посилатися на нього для обмежень)

--- com/study/test/ShipsOfWar.java
+++ com/study/test/ShipsOfWar.java
@@ -2,54 +2,37 @@ package com.study.test;
 
 import java.util.Scanner;
 
-
-
 public class ShipsOfWar  {
-    Ship ship = new Ship();
-    private static int readShipLength(Scanner scan) {
-        while (true) {
+    private static int readFieldLength(Scanner scan){
+        while (true){
             int value = scan.nextInt();
-            if (value > 0 & value <= 100) {
-                return value;
+            if (0<value & value<=100){
+                return value; } else {
+                System.out.println("Write a number from 1 to 100");
+                }
+            }
+        }
 
-            } else {
+    private static int readShipLength(Scanner scan){
+        while (true){
+            int value2 = scan.nextInt();
+            if (0<value2 & value2<=100){
+                return value2; } else {
                 System.out.println("Write a number from 1 to 100");
             }
         }
     }
-
-    private static int readFieldLength(Scanner scan) {
-        while (true) {
-            int value = scan.nextInt();
-            if (value > 0 & value <= 100) {
-                return value;
-
-            } else {
-                System.out.println("Write a number from 1 to 100");
-            }
-        }
-    }
-
 
      public static void main(String[] args) {
          Ship ship = new Ship();
 
-         System.out.println("Ведіть розмір поля");
-         readFieldLength(new Scanner(System.in));
-         ship.setShipLength(readFieldLength(new Scanner(System.in)));
+         System.out.println("Write a length of Field");
+         ship.setFieldLength(readFieldLength(new Scanner(System.in)));
 
-         System.out.println("Ведіть розмір корабля");
-         readShipLength(new Scanner(System.in));
+         System.out.println("Write a length of Ship");
          ship.setShipLength(readShipLength(new Scanner(System.in)));
-
-
-         ship.ship();
-
+         ship.fight();
         }
-
-
-
-
 }
 
 
